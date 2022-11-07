4bda1430775f1ea4a44fa0f741bdd3b51dcc75fa
Caio Luan Zenke <74631392+caiozenke@users.noreply.github.com>
2022-11-06 21:08:46 -0300

    aula sobre operadores condicionais em js

---
+++ condicionais.js
@@ -0,0 +1,38 @@
+// operador igual == (mesmo valor)
+// operador identico === (msmo valor e mesmo tipo)
+// operador diferente =!
+// operador nao indentico (valor diferente e tipo tbm)
+
+
+var num1 = 2
+var str1 = '1'
+if (num1 == str1){
+    document.write('oprado igual')
+} 
+else if (num1 === 2) {
+    document.write('operador identico ')
+} 
+else{
+    document.write('caiu no else')
+}
+
+
+
+// teste boletim 
+
+var n1 = 7
+var n2 = 4
+var n3 = 2
+
+var media_aluno = (n1 + n2 + n3) / 3
+
+
+if (media_aluno > 10){
+    alert('Erro Media Invalida');
+}
+else if (media_aluno >= 6){
+    document.write('Aluno Aprovado');
+}
+else{
+    document.write('Aluno Reprovado')
+}
