f2d9d5c07f1990a9d36f24c67c583726acf4858d
Caio Luan Zenke <74631392+caiozenke@users.noreply.github.com>
2022-11-18 20:08:27 -0300

    aula sobre switch

---
+++ switch.js
@@ -0,0 +1,19 @@
+var t1 = prompt('Digite um número')
+
+
+switch (parseInt(t1)){
+
+    case 1:
+        // faz comparação de identico e tudo se executado se nao colocar o break
+        document.write('Case 1 === Numero 1 ')
+        break
+    case 2:
+        document.write('Case 2 === Numero 2 ')
+
+        break
+    default:
+        document.write('Caiu no Default')
+        break
+}
+
+
