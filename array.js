ca8848c9e1119d7acfe4b190714d074819728f23
caiozenke <74631392+caiozenke@users.noreply.github.com>
2022-11-28 11:38:31 -0300

    array

---
+++ array.js
@@ -0,0 +1,30 @@
+var lista = Array();
+
+lista[1] = 'caio'
+lista[0] = '0'
+
+console.log(lista)
+
+
+// ou pode ser definida assim
+
+var lista_dif = []
+
+lista_dif[0] = 'indice 0'
+
+console.log(lista_dif)
+
+
+// var lista_frutas = Array()
+/*
+    var lista_frutas = []
+    lista_frutas[1] = 'Banana'
+    lista_frutas[2] = true
+    lista_frutas['x'] = 'Morango'
+    lista_frutas[4] = 100 
+ */
+ // var lista_frutas = Array('Banana', 'Maçã', 'Morango', 'Uva')
+    var lista_frutas = ['Banana', 'Maçã', 'Morango', 'Uva'] // atribuição automatica 0 cmc
+
+    
+    console.log(lista_frutas)
