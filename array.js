4e8fcfb06ab6860f1a4c4af78452beb0fb771e36
caiozenke <74631392+caiozenke@users.noreply.github.com>
2022-11-29 14:11:22 -0300

    Aula sobre Arrays

--- array.js
+++ array.js
@@ -3,7 +3,7 @@ var lista = Array();
 lista[1] = 'caio'
 lista[0] = '0'
 
-console.log(lista)
+//console.log(lista)
 
 
 // ou pode ser definida assim
@@ -12,7 +12,7 @@ var lista_dif = []
 
 lista_dif[0] = 'indice 0'
 
-console.log(lista_dif)
+//console.log(lista_dif)
 
 
 // var lista_frutas = Array()
@@ -24,7 +24,62 @@ console.log(lista_dif)
     lista_frutas[4] = 100 
  */
  // var lista_frutas = Array('Banana', 'Maçã', 'Morango', 'Uva')
-    var lista_frutas = ['Banana', 'Maçã', 'Morango', 'Uva'] // atribuição automatica 0 cmc
+    var lista_fruta = ['Banana', 'Maçã', 'Morango', 'Uva'] // atribuição automatica 0 cmc
 
     
-    console.log(lista_frutas)
+    //console.log(lista_frutas)
+   
+   
+    // Array multidimencional
+    var lista_de_coisas = []
+
+    
+               // lista dentro de uma lista
+   lista_de_coisas['pessoa'] = []
+
+
+            //definir um objeto dentro da lista pessoa
+   lista_de_coisas['pessoa'][1] = 'Caio'
+
+               //outro array dentro de um array
+   lista_de_coisas['carros'] = ['bmw','porshe','jesus']
+
+
+   console.log(lista_de_coisas)
+   console.log(lista_de_coisas['pessoa'][1])
+   console.log(lista_de_coisas['carros'][0])
+
+
+// incluir e excluir elementos
+
+lista_de_coisas['carros'].push('aston martin')// incluir no final do array
+lista_de_coisas['carros'].unshift('ferrari')// incluir no começo do array
+
+// Retorno  --->  carros: (5) ['ferrari', 'bmw', 'porshe', 'jesus', 'aston martin']
+
+lista_de_coisas['carros'].pop() // exclui o ultimo valor do array
+lista_de_coisas['carros'].shift()// exclui o primeiro
+
+//Retorno --->  carros: (3) ['bmw', 'porshe', 'jesus']
+
+// Pesquisa com INdexOf
+var lista_frutas = Array()
+
+lista_frutas[0] = 'Banana'
+lista_frutas[1] = 'Maça'
+lista_frutas[2] = 'Morango'
+lista_frutas[3] = 'Uva'
+
+console.log(lista_frutas.indexOf('Morango')) 
+console.log(lista_frutas.indexOf('Abacaxi')) //retorna -1
+
+var auxiliar = lista_frutas.indexOf('Banana')
+if(auxiliar === -1) {
+ console.log('Elemento não existe')
+} else {
+ console.log('Elemento existe e está na posição' + auxiliar)
+}
+
+//Lembre-se, a pesquisa é case sensitive!
+
+
